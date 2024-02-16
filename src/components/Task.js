f0576ddaabed9174f6de1546d91c85c1c4324494
sanjayryker <sanjaypandian01@gmail.com>
2024-02-16 14:23:03 +0530

    icon changed

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,5 @@
 
-import { FaEdit,FaCheckDouble,FaRegTrashAlt } from "react-icons/fa";
+import { FaEdit ,FaCheckDouble,FaRegTrashAlt } from "react-icons/fa";
 
 const Task = ({task,index,deleteTask,getSingleTask,setToComplete}) => {
   return (
@@ -9,8 +9,8 @@ const Task = ({task,index,deleteTask,getSingleTask,setToComplete}) => {
             {task.name}
         </p>
         <div className="task-icons">
-        <FaEdit color="green" onClick={() => {setToComplete(task)}}/>
-        <FaCheckDouble color="purple" onClick={() => getSingleTask(task)}/>
+        <FaCheckDouble  color="green" onClick={() => {setToComplete(task)}}/>
+        <FaEdit color="purple" onClick={() => getSingleTask(task)}/>
         <FaRegTrashAlt color="red" onClick={() => deleteTask(task._id)}/>
         </div>
     </div>
